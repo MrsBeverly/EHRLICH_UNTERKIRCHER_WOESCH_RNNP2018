d6f907f0c4ce9b24d8ee3443eb7a01ad80ec393a
Beverly Marion Ehrlich <bevehrlich@gmail.com>
2018-10-12 16:27:01 +0200

    Aufgabe2

--- src/Blatt2/Aufgabe3.java
+++ src/Blatt2/Aufgabe3.java
@@ -1,4 +1,38 @@
 package Blatt2;
+
+import java.util.Stack;
+
 /*WRITTEN BY EHRLICH BEVERLY, UNTERKIRCHER CHRISTOPH AND WÖSCH TIMON*/
 public class Aufgabe3 {
+
+    public class Consumer extends Thread {
+        private Stack<Integer> stack;
+
+        public Consumer(Stack<Integer> stack) {
+            this.stack = stack;
+        }
+
+        public void run() {
+            // TODO: implement!
+        }
+    }
+
+    public class Producer extends Thread {
+        private Stack<Integer> stack;
+
+        public Producer(Stack<Integer> stack) {
+            this.stack = stack;
+        }
+
+        public void run() {
+            // TODO: implement!
+        }
+    }
+
+    public class Main {
+        public static void main(String[] args) {
+            Stack<Integer> sharedStack = new Stack<>();
+            // TODO: implement
+        }
+    }
 }
