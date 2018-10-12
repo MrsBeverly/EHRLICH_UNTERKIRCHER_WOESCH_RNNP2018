6ad517ec9c466f26d72635060dd885bb7a5464b8
Beverly Marion Ehrlich <bevehrlich@gmail.com>
2018-10-12 16:24:34 +0200

    Aufgabe2

--- src/Blatt2/Aufgabe2.java
+++ src/Blatt2/Aufgabe2.java
@@ -1,8 +1,30 @@
 package Blatt2;
+
 /*WRITTEN BY EHRLICH BEVERLY, UNTERKIRCHER CHRISTOPH AND WÖSCH TIMON*/
 public class Aufgabe2 {
-//test
 
-    //test beverly
+    public class IntegerList {
+        protected int[] data;
+        protected int size;
+        private static final int DEFAULT_SIZE = 10;
+
+        public IntegerList() {
+            data = new int[DEFAULT_SIZE];
+        }
+
+        public IntegerList(IntegerList toCopy) {
+            /** * copy the original */
+        }
+
+        /**
+         * TODO:
+         *
+         * Implement:
+         * get(int index) – Return the integer stored at index
+         * add(int value) – Add a new value to the integer list
+         * clear() – Deletes all integers * setCapacity(int n) – Reallocates the data array increasing or decreasing its size to n
+         * toArray() – Returns a copy of the integer list and returns it
+         * */
+    }
 }
 
