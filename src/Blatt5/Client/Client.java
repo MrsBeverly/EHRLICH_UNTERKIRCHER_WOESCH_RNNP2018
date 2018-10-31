b9fea7fd779de1a92f099d1bd7f17826da55953f
chrisunterkircher@gmail.com <performerrx1705>
2018-10-31 11:13:01 +0100

    test chris

--- src/Blatt5/Client/Client.java
+++ src/Blatt5/Client/Client.java
@@ -29,6 +29,66 @@ public class Client {
 
 
 
+    public void connect(String host) throws IOException {
+        connect(host, DEFAULT_PORT);
+    }
+
+
+
+    public boolean isConnected() {
+        return socket != null && socket.isConnected();
+    }
+
+
+    public void disconnect() throws IOException {
+        if (!isConnected())
+            throw new IllegalStateException("Not connected to a host");
+        socket.close();
+        reader = null;
+        writer = null;
+        if (debug)
+            System.out.println("Disconnected from the host");
+    }
+
+
+
+
+    protected String readResponseLine() throws IOException{
+        String response = reader.readLine();
+        if (debug) {
+            System.out.println("DEBUG [in] : " + response);
+        }
+        if (response.startsWith("-ERR"))
+            throw new RuntimeException("Server has returned an error: " + response.replaceFirst("-ERR ", ""));
+        return response;
+    }
+
+
+
+
+
+    protected String sendCommand(String command) throws IOException {
+        if (debug) {
+            System.out.println("DEBUG [out]: " + command);
+        }
+        writer.write(command + "\n");
+        writer.flush();
+        return readResponseLine();
+    }
+
+
+
+
+    public void connect(String host, int port) throws IOException {
+//…
+        readResponseLine();
+    }
+
+
+
+
+
+
 
 
 //End Class
