05ae76c5989d6245823ea1a5b130960d3b594cb0
Timon <woesch09@gmail.com>
2018-11-12 14:32:25 +0100

    MULTITHREAD --Timon Woesch

--- src/Blatt6/HTTPServer_single.java
+++ src/Blatt6/HTTPServer_single.java
@@ -9,28 +9,21 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class HTTPServer_single {
+public class HTTPServer_single extends Thread{
     private static Boolean debug = true;
     private static final String Path2DocumentRoot ="src/Blatt6/documentRoot";
+    private Socket socket;
 
-    public static void main(String[] args) throws Exception {
-        Server server;
-        ServerSocket welcomeSocket = new ServerSocket(8800);
+    public HTTPServer_single(Socket socket_in){
+        socket=socket_in;
+    }
 
-
-        while (true) {
-            //waiting for a new client
-            Socket connectionSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
-            DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+    public void run() {
+        try {
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
             String in;
-            File file;
-            BufferedReader br;
-            Boolean flag = false;
             String[] split_in;
-
-            HTTPServer_single myHTTPServer = new HTTPServer_single();
-
 
             in = inFromClient.readLine();
             if (debug) System.out.println("[DEBUG] in  = " + in);
@@ -38,38 +31,59 @@ public class HTTPServer_single {
             split_in = in.split(" ");
 
             if (split_in[1].equals("/")) {
+                getIndex(outToClient);
 
-                //file = new File("C:\\Users\\Timon\\Desktop\\Uni\\S7\\Rechnernetze und Netzwerktechnik\\UE\\EHRLICH_UNTERKIRCHER_WOESCH_RNNP2018\\src\\Blatt6\\documentRoot\\index.html");
-                file = new File(Path2DocumentRoot + "/index.html");
-                br = new BufferedReader(new FileReader(file));
-
-                outToClient.write("HTTP/0.9 200 OK\r\n\r\n".getBytes("UTF-8"));
-
-                while (!flag) {
-                    String string = br.readLine();
-                    if (debug) System.out.println("[DEBUG] out = " + string);
-
-                    if (string == null) {
-                        flag = true;
-                    } else {
-                        outToClient.write(string.getBytes("UTF-8"));
-                    }
-                }
             } else if (split_in[1].endsWith(".png")) {
-                myHTTPServer.sendNudes(Path2DocumentRoot + split_in[1], "gif", outToClient);
+                getFile(Path2DocumentRoot + split_in[1], "gif", outToClient);
 
             } else if (split_in[1].endsWith(".gif")) {
-                myHTTPServer.sendNudes(Path2DocumentRoot + split_in[1], "png", outToClient);
+                getFile(Path2DocumentRoot + split_in[1], "png", outToClient);
 
             } else if (split_in[1].endsWith(".jpg")) {
-                myHTTPServer.sendNudes(Path2DocumentRoot + split_in[1], "jpg", outToClient);
+                getFile(Path2DocumentRoot + split_in[1], "jpg", outToClient);
+            }
 
+            socket.close();
+        }catch (Exception e){
+            //Exception
+        }
+    }
+
+    private void getIndex(DataOutputStream outToClient) throws IOException {
+        File file;
+        BufferedReader br;//file = new File("C:\\Users\\Timon\\Desktop\\Uni\\S7\\Rechnernetze und Netzwerktechnik\\UE\\EHRLICH_UNTERKIRCHER_WOESCH_RNNP2018\\src\\Blatt6\\documentRoot\\index.html");
+        file = new File(Path2DocumentRoot + "/index.html");
+        br = new BufferedReader(new FileReader(file));
+        Boolean flag = false;
+
+        outToClient.write("HTTP/0.9 200 OK\r\n\r\n".getBytes("UTF-8"));
+
+        while (!flag) {
+            String string = br.readLine();
+            if (debug) System.out.println("[DEBUG] out = " + string);
+
+            if (string == null) {
+                flag = true;
+            } else {
+                outToClient.write(string.getBytes("UTF-8"));
             }
         }
     }
 
+    public static void main(String[] args) throws Exception {
+        HTTPServer_single server;
+        ServerSocket welcomeSocket = new ServerSocket(8800);
 
-    public void sendNudes(String path,String fileType, DataOutputStream myOut) throws IOException {
+
+        while (true) {
+            //waiting for a new client
+            Socket connectionSocket = welcomeSocket.accept();
+            server= new HTTPServer_single(connectionSocket);
+            server.start();
+        }
+    }
+
+    public void getFile(String path, String fileType, DataOutputStream myOut) throws IOException {
 
         File input_File = new File(path);
         BufferedImage myImage = ImageIO.read(input_File);
