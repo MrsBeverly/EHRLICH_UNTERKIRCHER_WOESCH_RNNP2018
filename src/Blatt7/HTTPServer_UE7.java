db6f3bebb57596f838f17a53fddcb6c3c5c0a17f
Timon <woesch09@gmail.com>
2018-11-15 19:53:16 +0100

    Blatt7 updated --Timon Woesch

--- src/Blatt7/HTTPServer_UE7.java
+++ src/Blatt7/HTTPServer_UE7.java
@@ -1,15 +1,16 @@
 /*WRITTEN BY EHRLICH BEVERLY, UNTERKIRCHER CHRISTOPH AND WÖSCH TIMON*/
-package Blatt6;
+package Blatt7;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.StringTokenizer;
 
 public class HTTPServer_UE7 extends Thread{
     private static Boolean debug = true;
-    private static final String Path2DocumentRoot ="src/Blatt6/documentRoot";
+    private static String path2DocumentRoot ="src/Blatt6/documentRoot";
     private Socket socket;
 
     public HTTPServer_UE7(Socket socket_in){
@@ -18,33 +19,29 @@ public class HTTPServer_UE7 extends Thread{
 
     public void run() {
         try {
-            //wichtig ist bufferedReader
             BufferedReader inFromClient = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             DataOutputStream outToClient = new DataOutputStream(socket.getOutputStream());
             String in;
-            String[] split_in;
+            StringTokenizer in_tokens;
 
             in = inFromClient.readLine();
             if (debug) System.out.println("[DEBUG] in  = " + in);
 
-            //sollte man eher mit tokenizer machen
-            //prüfen, ob alle 3 tokens auch vorhanden sind!!!
-            //sonst fehler! hier prüfen
-            split_in = in.split(" ");
+            in_tokens = new StringTokenizer(in);
 
-            if (split_in[1].equals("/")) {//root pfad -> /index.html
-                //print index.html
-                getIndex(outToClient);
-
-            } else if (split_in[1].endsWith(".png")) {
-                getFile(Path2DocumentRoot + split_in[1], "gif", outToClient);
-
-            } else if (split_in[1].endsWith(".gif")) {
-                getFile(Path2DocumentRoot + split_in[1], "png", outToClient);
-
-            } else if (split_in[1].endsWith(".jpg")) {
-                getFile(Path2DocumentRoot + split_in[1], "jpg", outToClient);
+            //checking if there are 3 Tokens
+            if(in_tokens.countTokens()!=3){
+                socket.close();
+                System.exit(-1);
             }
+
+            in_tokens.nextToken();
+
+            // GET actions
+            GETActions(outToClient, in_tokens.nextToken());
+
+            // POST actions
+            // TODO POST Actions
 
             socket.close();
         }catch (Exception e){
@@ -52,25 +49,40 @@ public class HTTPServer_UE7 extends Thread{
         }
     }
 
+    private void GETActions(DataOutputStream outToClient, String s) throws IOException {
+        if (s.equals("/")) {
+            //root pfad -> /index.html
+            //print index.html
+            getIndex(outToClient);
+        } else if (s.endsWith(".png")) {
+            getFile(path2DocumentRoot + s, "gif", outToClient);
+        } else if (s.endsWith(".gif")) {
+            getFile(path2DocumentRoot + s, "png", outToClient);
+        } else if (s.endsWith(".jpg")) {
+            getFile(path2DocumentRoot + s, "jpg", outToClient);
+        }
+    }
+
     private void getIndex(DataOutputStream outToClient) throws IOException {
         File file;
         BufferedReader br;
-        //file = new File("C:\\Users\\Timon\\Desktop\\Uni\\S7\\Rechnernetze und Netzwerktechnik\\UE\\EHRLICH_UNTERKIRCHER_WOESCH_RNNP2018\\src\\Blatt6\\documentRoot\\index.html");
-        file = new File(Path2DocumentRoot + "/index.html");
+        file = new File(path2DocumentRoot + "/index.html");
         br = new BufferedReader(new FileReader(file));
         Boolean flag = false;
 
-
         //info:  readline frisst crlf auf
-
 
         //sobald man diese "OK" message schickt, erwartet sich der browser einen header
         //das ist weil kein browser mehr mit 0.9 läuft
-        outToClient.write("HTTP/0.9 200 OK\r\n\r\n".getBytes("UTF-8"));//zwei CLRF weil die headerfiles nicht da sind; bei höherer Implementierung kommt hier die headerfile
+
+        //Status response
+        outToClient.write("HTTP/0.9 200 OK\r\n".getBytes("UTF-8"));
+        //header
+        outToClient.write("\r\n".getBytes("UTF-8"));
 
         while (!flag) {
             String string = br.readLine();
-            if (debug) System.out.println("[DEBUG] out = " + string);
+            if (debug) System.out.println("[DEBUG] " + socket + " out = " + string);
 
             if (string == null) {
                 flag = true;
@@ -81,17 +93,23 @@ public class HTTPServer_UE7 extends Thread{
     }
 
     public static void main(String[] args) throws Exception {
-        HTTPServer_UE7 server;
-        ServerSocket welcomeSocket = new ServerSocket(8800);
+        //checking args
+        if(args.length!=2){
+            System.out.println("Port and Document root necessary");
+            System.exit(-1);
+        }
+        if(debug) System.out.println("[DEBUG] MAIN: Port = " + args[0] + "\n[DEBUG] MAIN: Document root = " + args[1]);
 
-        //von commandline anlesen
-        //Path2DocumentRoot = args[0];
-        //port = args[1];
+        //setting up server
+        HTTPServer_UE7 server;
+        ServerSocket welcomeSocket = new ServerSocket(Integer.valueOf(args[0]));
+        path2DocumentRoot = args[1];
 
         while (true) {
             //waiting for a new client
             Socket connectionSocket = welcomeSocket.accept();
             server= new HTTPServer_UE7(connectionSocket);
+            if(debug) System.out.println("[DEBUG] MAIN: Thread with Socket = " + connectionSocket + " starting");
             server.start();
         }
     }
@@ -105,11 +123,15 @@ public class HTTPServer_UE7 extends Thread{
 
         //kopiert direkt binär auf outputstream
         //file.copy(path, destinationStream) geht auch
-        //destinationStream = myOut
+        //destinationStream = myOuts
         ImageIO.write(myImage,fileType,myByteArrOutStr);
 
-        myOut.write("HTTP/0.9 200 OK\r\n\r\n".getBytes("UTF-8"));
+        //status
+        myOut.write("HTTP/0.9 200 OK\r\n".getBytes("UTF-8"));
+        //header
+        myOut.write("\r\n".getBytes("UTF-8"));
 
+        if(debug) System.out.println("[DEBUG] " + socket + " out: Returning File");
 
         myOut.write(myByteArrOutStr.toByteArray());
         myOut.flush();
